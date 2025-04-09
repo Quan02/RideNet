149e0556c37d3941a416ede1e423e54df77ae0da
FuQuan <amfuquan@gmail.com>
2025-04-09 12:02:14 +0800

    Add distance to graph edge

--- src/data-preprocessing/validate.py
+++ src/data-preprocessing/validate.py
@@ -1,14 +1,13 @@
 """"This module validates the road network data and provide more information."""
 
 from pathlib import Path
-import pickle
 
 import networkx as nx
 
 from preprocessing_utils import get_logger, load_graph
 
 logger = get_logger(__name__) 
-EXTRACTED_GRAPH_FILE = Path("data/processed/road_network_processed.pkl")
+EXTRACTED_GRAPH_FILE = Path("data/processed/road_network_distance.pkl")
 
 def validate_graph(graph: nx.MultiDiGraph) -> None:
     """
@@ -24,11 +23,14 @@ def validate_graph(graph: nx.MultiDiGraph) -> None:
     for u, v, data in graph.edges(data=True):
         if 'geometry' not in data:
             logger.warning(f"Edge {u} -> {v} is missing geometry.")
+        if 'distance' not in data:
+            logger.warning(f"Edge {u} -> {v} is missing geometry.")
     if nx.is_weakly_connected(graph):
         logger.info("Graph is connected.")
     else:
         components = list(nx.weakly_connected_components(graph))
         logger.warning(f"Graph has {len(components)} disconnected components.")
+
 
 if __name__ == "__main__":
     road_graph = load_graph(EXTRACTED_GRAPH_FILE)
